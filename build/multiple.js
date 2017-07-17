6c4115e2c657e6d4fa372b70b4e331554d3df639
brothercake <cake@brothercake.com>
2017-07-17 18:19:24 +0100

    add v3.0 build

---
+++ build/multiple.js
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ Copyright (c) 2013-7 AccessibilityOz        http://www.accessibilityoz.com.au/
+ ------------------------------------------------------------------------------
+ OzPlayer [3.0] => batch configuration script
+ ------------------------------------------------------------------------------
+*******************************************************************************/
+(function()
+{
+
+
+    //initialise all OzPlayer instances
+    //n.b. you should initialise players as soon as possible after the markup
+    //i.e. don't wait for window.onload or DOMContentLoaded or suchlike
+    OzPlayer.init();
+
+
+})();
